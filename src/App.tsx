f19a4ed5b6aadf9343795a66da833457bc9fdb96
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-09-14 19:18:57 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import SignupPage from "./pages/SignupPage";
 import { WalletProvider } from "./contexts/WalletContext";
 import { LoginProvider } from "./contexts/LoginContext";
 import SurveyorHome from "./pages/surveyor/SurveyorHome";
+import { ToastProvider } from "./contexts/ToastContext";
 
 export default function App() {
   return (
     <div className="font-spectral w-full min-h-screen h-full bg-black">
-      <LoginProvider>
-        <WalletProvider>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/surveyor" element={<SurveyorHome />} />
-          </Routes>
-        </WalletProvider>
-      </LoginProvider>
+      <ToastProvider>
+        <LoginProvider>
+          <WalletProvider>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/surveyor" element={<SurveyorHome />} />
+            </Routes>
+          </WalletProvider>
+        </LoginProvider>
+      </ToastProvider>
     </div>
   );
 }
