0dfd812dfa3cbe914da7246e061295f8cc16cc92
VishSeran <vishwa.sovis@gmail.com>
2025-11-01 00:18:29 +0530

    contact updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,23 @@
-const contact = () =>{
-    return(
-        <div>Contact Page</div>
+function contact() {
+    return (
+        <div className="min-h-screen p-4 sm:p-6">
+            <div className="max-w-7xl mx-auto">
+                <div className="mb-6 bg-gradient-to-r from-green-50 to-emerald-50 rounded-2xl p-6 border border-green-200">
+                    <h1 className="text-2xl sm:text-3xl font-bold text-black mb-2">Get in Touch</h1>
+                    <p className="text-gray-700 text-justify"> We’re here to assist Surveyors, IVSL officers, and Notaries with any inquiries or support regarding property verification, title validation, and blockchain transaction updates.</p>
+                </div>
+
+                 <p className="text-gray-600 text-justify">
+                    For technical issues, account assistance, or process clarifications, please reach out to our support team at  
+                    <span className="font-semibold text-emerald-700"> support@realestatechain.lk </span>
+                     or call us at <span className="font-semibold text-emerald-700"> +94 71 234 5678</span>.  
+                    We’ll ensure your concerns are handled promptly and securely.
+                 </p>
+            </div>
+
+        </div>
+
     )
-};
+}
 
 export default contact;
